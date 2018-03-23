78c4b120947a900d192c856bb9d670981b65b88a
jannes <jannes.magnusson@emtec.de>
2018-03-23 12:33:49 +0100

    setup default angular with electron

---
+++ Songsheet/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title: String;
+
+  constructor() {
+    var app = electron.remote.app;
+    this.title = app.getAppPath();
+  }
+}
---
+++ Songsheet/src/typings.d.ts
@@ -0,0 +1,7 @@
+/* SystemJS module definition */
+declare var module: NodeModule;
+declare var electron: any;
+
+interface NodeModule {
+  id: string;
+}
