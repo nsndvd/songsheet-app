0aed9505682a29bc5991150cb7aa32fa6e27ff6e
nilsroesel <nils.roesel@gmail.com>
2018-09-21 20:21:37 +0200

    Add typeguard and factory method to Song

--- src/app/models/song.ts
+++ src/app/models/song.ts
@@ -10,6 +10,37 @@ export class Song {
   maxLineWidth: number = 0;
   preview?: string;
 
+  static isDefinedNonNullSong(val: any): val is Song {
+    if(val) {
+      let is: boolean = true;
+      is = is && (val.id? typeof val.id === 'string': !val.id);
+      is = is && typeof val.title === 'string';
+      is = is && (val.artist? typeof val.artist === 'string': !val.artist);
+      is = is && (val.bpm? typeof val.bpm === 'number': !val.bpm);
+      is = is && (val.books? val.id instanceof Array: !val.books);
+      is = is && (val.order? val.id instanceof Array: !val.order);
+      is = is && (val.annotationCells? typeof val.annotationCells === 'number': !val.annotationCells);
+      return is && (val.maxLineWidth? typeof val.maxLineWidth === 'number': !val.maxLineWidth);
+    } else return false;
+  }
+
+  static from(val: any): Song {
+    if(Song.isDefinedNonNullSong(val)) {
+      const song: Song = new Song();
+      song.id = (val as Song).id;
+      song.title = (val as Song).title;
+      song.artist = (val as Song).artist;
+      song.bpm = (val as Song).bpm;
+      song.books = (val as Song).books;
+      song.blocks = (val as Song).blocks;
+      song.order = (val as Song).order;
+      song.annotationCells = (val as Song).annotationCells;
+      song.maxLineWidth = (val as Song).maxLineWidth;
+      return song;
+    }
+    else return null;
+  }
+
   constructor(song?: string | any){
     if(typeof song === 'string'){
       this.title = song;
